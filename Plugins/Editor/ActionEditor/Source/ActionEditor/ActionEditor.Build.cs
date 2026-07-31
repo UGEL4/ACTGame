e5adba8860ba8e9cc4a240b4489655044a21128c
UGEL4 <1365972041@qq.com>
2026-07-31 18:27:25 +0800

    update

--- Plugins/Editor/ActionEditor/Source/ActionEditor/ActionEditor.Build.cs
+++ Plugins/Editor/ActionEditor/Source/ActionEditor/ActionEditor.Build.cs
@@ -18,6 +18,7 @@ public class ActionEditor : ModuleRules
 		PrivateIncludePaths.AddRange(
 			new string[] {
 				// ... add other private include paths required here ...
+				"ActionEditor"
 			}
 			);
 			
---
+++ Source/ACTGame/ActionEditor/Actor/HitBox/HitBoxActor.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ActionEditor/Actor/HitBox/HitBoxActor.h"
+#include "Components/CapsuleComponent.h"
+
+
+// Sets default values
+AHitBoxActor::AHitBoxActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	HitBox = CreateDefaultSubobject<UCapsuleComponent>(TEXT("HitBox"));
+    HitBox->InitCapsuleSize(34.0f, 88.0f);
+    HitBox->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
+    HitBox->SetHiddenInGame(false);
+
+    HitBox->SetShouldUpdatePhysicsVolume(false);
+    HitBox->SetCanEverAffectNavigation(false);
+    RootComponent = HitBox;
+
+}
+
+// Called when the game starts or when spawned
+void AHitBoxActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHitBoxActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
